a322dfd2280e42907a1b0c718883b03e68e49733
Caleb D <89933841+CFMCGEE@users.noreply.github.com>
2023-02-19 21:35:13 -0500

    Update update_confluence_page.py

--- update_confluence_page.py
+++ update_confluence_page.py
@@ -3,6 +3,7 @@
 The following should be modified for script to work efficiently:
 - PUT_EXCEL_SHEET_LOCATION_HERE
 - PUT_CREDENTIALS_LOCATION_HERE
+  - Use the "CREDENTIALS_FILE" template in neighoring file 
 - MASTER_FOLDER
 
  """
@@ -18,7 +19,7 @@ from datetime import datetime, date
 from bs4 import BeautifulSoup as BS
 
 # Load the Excel file
-path_to_credentials = 'PUT_CREDENTIALS_LOCATION_HERE'
+path_to_credentials = 'PUT_CREDENTIALS_LOCATION_HERE' # 
 path_to_excel_sheet = 'PUT_EXCEL_SHEET_LOCATION_HERE'
 wb = openpyxl.load_workbook(filename=path_to_excel_sheet)
 ws = wb.active
